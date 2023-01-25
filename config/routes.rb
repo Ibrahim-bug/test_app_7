242d66003b173da103a7f6c9fe86459341c0517b
Ibrahim <ibrahimkashif97@gmail.com>
2023-01-25 16:19:02 +0500

    Add about paget

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "pages#hello"
+  root "pages#home"
 
+  get "about",to: "pages#about"
 end
