fed27633bca768d5b3fd4892a91a03ca7abaf2be
Ibrahim <ibrahimkashif97@gmail.com>
2023-02-09 16:46:43 +0500

    Add users create action

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "pages#home"
-
   get "about",to: "pages#about"
-
   resources :articles
+  get "signup", to: "users#new"
+  resources :users, except: [:new]
 end
 
 
