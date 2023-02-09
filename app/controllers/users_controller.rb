3f43f63d177f63b7604c7cacb8ebddb50c19b20b
Ibrahim <ibrahimkashif97@gmail.com>
2023-02-09 17:17:13 +0500

    Add edit user functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,13 @@
 class UsersController < ApplicationController
+  before_action  :set_user, only: [ :edit, :update]
+
   def new
     @user = User.new
   end
+
+  def edit
+  end
+
   def create
     @user = User.new(users_params)
     @user.save
@@ -13,11 +19,23 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+    if @user.update(users_params)
+      flash[:notice] = "Your details has been updated successfully."
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+
+
   private
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def users_params
     params.require(:user).permit(:username,:email,:password)
   end
-
 
 end
